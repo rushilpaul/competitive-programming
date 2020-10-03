e12088b25e1e1136e9601e9ba41eaab972048d61
Rushil Paul <paurushi@amazon.com>
2020-10-03 14:24:08 +0530

    [Codeforces] 672 Div2

---
+++ contests/codeforces/672_div2/D.java
@@ -0,0 +1,395 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class D {
+
+    static int mod = 998244353;
+    static int[] fact, invFact;
+
+    static int ncr(int n, int r) {
+        if(r < 0 || n < 0 || n - r < 0) return 0;
+        return (int) ( 1L * fact[n] * invFact[r] % mod * invFact[n - r] % mod );
+    }
+
+    public static void main(String[] args) {
+
+        FastScanner sc = new FastScanner();
+        int n = sc.nextInt(), k = sc.nextInt();
+        fact = new int[n + 1];
+        invFact = new int[n + 1];
+        fact[0] = 1;
+        for(int i = 1; i <= n; i++) {
+            fact[i] = (int) (1L * fact[i-1] * i % mod);
+            invFact[i] = pow(fact[i], mod - 2, mod);
+        }
+        invFact[0] = invFact[1];
+
+        Seg[] intervals = new Seg[2 * n];
+
+        int j = 0;
+        for(int i = 0; i < n; i++) {
+            int x = sc.nextInt(), y = sc.nextInt();
+            intervals[j++] = new Seg(x, true);
+            intervals[j++] = new Seg(y, false);
+        }
+        sort(intervals);
+
+        int ans = 0, started = 0, ended = 0;
+
+        for(int i = 0; i < j; i++) {
+
+            if(intervals[i].start) {
+
+                int size = started - ended;
+                ans += ncr(size, k - 1);
+                if (ans >= mod)
+                    ans %= mod;
+                started++;
+            } else
+                ended++;
+        }
+        println(ans + "");
+    }
+
+    static class Seg implements Comparable<Seg> {
+        int x;
+        boolean start;
+
+        public Seg(int x, boolean start) {
+            this.x = x;
+            this.start = start;
+        }
+
+        @Override
+        public int compareTo(Seg o) {
+            if(o.x == x) {
+                if(start == o.start)
+                    return 0;
+                else if (start)
+                    return -1;
+                else
+                    return 1;
+            }
+            return x - o.x;
+        }
+    }
+
+    static final Random random = new Random();
+
+    static class FastScanner {
+        private InputStream sin = System.in;
+        private final byte[] buffer = new byte[1024];
+        private int ptr = 0;
+        private int buflen = 0;
+        public FastScanner(){}
+        public FastScanner(String filename) throws FileNotFoundException {
+            File file = new File(filename);
+            sin = new FileInputStream(file);
+        }
+        private boolean hasNextByte() {
+            if (ptr < buflen) {
+                return true;
+            }else{
+                ptr = 0;
+                try {
+                    buflen = sin.read(buffer);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (buflen <= 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private int readByte() { if (hasNextByte()) return buffer[ptr++]; else return -1;}
+        private static boolean isPrintableChar(int c) { return 33 <= c && c <= 126;}
+        public boolean hasNext() { while(hasNextByte() && !isPrintableChar(buffer[ptr])) ptr++; return hasNextByte();}
+        public String next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            StringBuilder sb = new StringBuilder();
+            int b = readByte();
+            while(isPrintableChar(b)) {
+                sb.appendCodePoint(b);
+                b = readByte();
+            }
+            return sb.toString();
+        }
+        public long nextLong() {
+            if (!hasNext()) throw new NoSuchElementException();
+            long n = 0;
+            boolean minus = false;
+            int b = readByte();
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+            if (b < '0' || '9' < b) {
+                throw new NumberFormatException();
+            }
+            while(true){
+                if ('0' <= b && b <= '9') {
+                    n *= 10;
+                    n += b - '0';
+                }else if(b == -1 || !isPrintableChar(b) || b == ':'){
+                    return minus ? -n : n;
+                }else{
+                    throw new NumberFormatException();
+                }
+                b = readByte();
+            }
+        }
+        public int nextInt() {
+            long nl = nextLong();
+            if (nl < Integer.MIN_VALUE || nl > Integer.MAX_VALUE) throw new NumberFormatException();
+            return (int) nl;
+        }
+        public double nextDouble() { return Double.parseDouble(next());}
+        public long[] nextLongArray(final int n){
+            final long[] a = new long[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextLong();
+            return a;
+        }
+        public int[] nextIntArray(final int n){
+            final int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+        public double[] nextDoubleArray(final int n){
+            final double[] a = new double[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextDouble();
+            return a;
+        }
+
+        public List<Integer>[] adjacencyList(int nodes, int edges) {
+            return adjacencyList(nodes, edges, false);
+        }
+        public List<Integer>[] adjacencyList(int nodes, int edges, boolean isDirected) {
+
+            List<Integer>[] adj = new ArrayList[nodes + 1];
+            Arrays.setAll(adj, (i) -> new ArrayList<>());
+            for (int i = 0; i < edges; i++) {
+                int a = nextInt(), b = nextInt();
+                adj[a].add(b);
+                if (!isDirected) adj[b].add(a);
+            }
+            return adj;
+        }
+    }
+
+    static class FastWriter {
+
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        void print(int n) throws Exception { writer.write(n + ""); }
+        void println(int n) throws Exception { writer.write(n + "\n"); }
+
+        void print(long n) throws Exception { writer.write(n + ""); }
+        void println(long n) throws Exception { writer.write(n + "\n"); }
+
+        void print(String s) throws Exception { writer.write(s); }
+        void println(String s) throws Exception { writer.write(s); writer.newLine(); }
+
+        void println(int ar[]) throws Exception { for(int e : ar) writer.write(e + " "); writer.newLine(); }
+        void println(long ar[]) throws Exception { for(long e : ar) writer.write(e + " "); writer.newLine(); }
+        void println(double ar[]) throws Exception { for(double e : ar) writer.write(e + " "); writer.newLine(); }
+        void close() throws Exception { writer.close(); }
+        <T> void println(List<T> L) throws Exception { for(T o : L) writer.write(o + " "); writer.newLine(); }
+    }
+
+    static int findDistance(List<Integer> G[], int nodes, int src, int dst) {
+
+        LinkedList<Integer> queue = new LinkedList<>();
+        int dist[] = new int[nodes + 1];
+        int ans = -1;
+        dist[src] = 0;
+        queue.add(src);
+        while(queue.size() > 0) {
+            int u = queue.poll();
+            if(u == dst) {
+                ans = dist[u];
+            }
+            for(int v : G[u]) {
+                if(dist[v] >= 0)
+                    continue;
+                queue.add(v);
+                dist[v] = dist[u] + 1;
+            }
+        }
+        return ans;
+    }
+
+    static void shuffleList(List<Integer> A) {
+        int n = A.size();
+        for(int i = 0; i < n; i++) {
+            int tmp = A.get(i);
+            int randomPos = i + random.nextInt(n-i);
+            A.set(i, A.get(randomPos));
+            A.set(randomPos, tmp);
+        }
+    }
+
+    static void sort(List<Integer> A) {
+        shuffleList(A);
+        Collections.sort(A);
+    }
+
+    static <T> void sort(T[] A) {
+        shuffleArray(A);
+        Arrays.sort(A);
+    }
+
+    static void sort(int A[]) {
+        shuffleArray(A);
+        Arrays.sort(A);
+    }
+    static void sort(long A[]) {
+        shuffleArray(A);
+        Arrays.sort(A);
+    }
+    static void sort(double A[]) {
+        shuffleArray(A);
+        Arrays.sort(A);
+    }
+
+    static <T> void shuffleArray(T[] A) {
+        int n = A.length;
+        for(int i = 0; i < n; i++) {
+            T tmp = A[i];
+            int randomPos = i + random.nextInt(n-i);
+            A[i] = A[randomPos];
+            A[randomPos] = tmp;
+        }
+    }
+
+    static void shuffleArray(int[] A) {
+        int n = A.length;
+        for(int i = 0; i < n; i++) {
+            int tmp = A[i];
+            int randomPos = i + random.nextInt(n-i);
+            A[i] = A[randomPos];
+            A[randomPos] = tmp;
+        }
+    }
+    static void shuffleArray(long[] A) {
+        int n = A.length;
+        for(int i = 0; i < n; i++) {
+            long tmp = A[i];
+            int randomPos = i + random.nextInt(n-i);
+            A[i] = A[randomPos];
+            A[randomPos] = tmp;
+        }
+    }
+    static void shuffleArray(double[] A) {
+        int n = A.length;
+        for(int i = 0; i < n; i++) {
+            double tmp = A[i];
+            int randomPos = i + random.nextInt(n-i);
+            A[i] = A[randomPos];
+            A[randomPos] = tmp;
+        }
+    }
+
+    static int[] subArray(int A[], int x, int y) {
+        int B[] = new int[y - x + 1];
+        for(int i = x; i <= y; i++)
+            B[i-x] = A[i];
+        return B;
+    }
+
+    static int[] toArray(List<Integer> L) {
+        return L.stream().mapToInt(x -> x).toArray();
+    }
+
+    static void println(int[] A) {
+        for(int e: A) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    static void println(long[] A) {
+        for(long e: A) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    static void println(List arr) {
+        for(Object e: arr) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    static void print(String s) {
+        System.out.print(s);
+    }
+
+    static void println(String s) {
+        System.out.println(s);
+    }
+
+    static List<Integer> toList(int ar[]) {
+        return Arrays.stream(ar).boxed().collect(Collectors.toList());
+    }
+
+    static List<Long> toList(long ar[]) {
+        return Arrays.stream(ar).boxed().collect(Collectors.toList());
+    }
+
+    static List<Double> toList(double ar[]) {
+        return Arrays.stream(ar).boxed().collect(Collectors.toList());
+    }
+
+    static long gcd(long a, long b) {
+        if(b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+
+    private static int abs(int a){  return (a>=0) ? a: -a;  }
+    private static int min(int... ins){ return Arrays.stream(ins).min().getAsInt(); }
+    private static int max(int... ins){ return Arrays.stream(ins).max().getAsInt(); }
+    private static int sum(int... ins){ return Arrays.stream(ins).sum(); }
+    private static long abs(long a){ return (a>=0) ? a: -a; }
+    private static long min(long... ins){ return Arrays.stream(ins).min().getAsLong(); }
+    private static long max(long... ins){ return Arrays.stream(ins).max().getAsLong(); }
+    private static long sum(long... ins){ return Arrays.stream(ins).sum(); }
+    private static double abs(double a){ return (a>=0) ? a: -a; }
+    private static double min(double... ins){ return Arrays.stream(ins).min().getAsDouble(); }
+    private static double max(double... ins){ return Arrays.stream(ins).max().getAsDouble(); }
+    private static double sum(double... ins){ return Arrays.stream(ins).sum(); }
+
+    private static class Pair implements Comparable<Pair> {
+        int x, y;
+        Pair(int x, int y) { this.x = x; this.y = y; }
+        Pair() {}
+
+        @Override
+        public int compareTo(Pair other) {
+            return x == other.x ? y - other.y : x - other.x;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Pair pair = (Pair) o;
+            return x == pair.x &&
+                    y == pair.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+    }
+
+    static int pow(int n, int p, int mod) {
+        if(p == 0)
+            return 1;
+        long x = pow(n, p/2, mod);
+        x = x * x % mod;
+        return (int) ( p % 2 == 0 ? x : x * n % mod );
+    }
+}
+
+
